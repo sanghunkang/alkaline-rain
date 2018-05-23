fccad145f9880aad1c4cb698833ba5e39d9688f3
sanghunkang <sanghunkang.dev@gmail.com>
2018-05-23 22:55:38 +0900

    minor revision

--- embeddingFeeder.py
+++ embeddingFeeder.py
@@ -19,27 +19,34 @@ class EmbeddingFeeder():
     def __init__(self, initial_seed=10):
         self.seed = initial_seed
         self.arr_point = []
+        self.arr_seed_used = []
         # self.some_store for used keys
 
         # Initialisation actions
         for i in range(NUM_WORD):
-            word = self._get_random_word()
-            embedding = self._get_embedding(word)
+            self._set_seed()
+            seed = self._get_seed()
+            word = self._fetch_random_word(seed)
+            embedding = self._fetch_embedding(word)
             self.arr_point.append({"word": word, "embedding": embedding, "status": 1})
 
-    def _update_seed(self):
-        self.seed += 1
-
-    def _get_random_word(self):
+    def _set_seed(self):
         # Some mechanism to control random seed including
         #   - check repeated inputs
         #   - update level
-        seed = randint(0, SIZE_VOCABULARY)
-        word = r.get(seed).decode("utf-8")
+        if True:
+            self.seed = randint(0, SIZE_VOCABULARY)
+
+    def _get_seed(self):
+        return self.seed
+
+    def _fetch_random_word(self):
+        self._set_seed()
+        word = r.get(self.seed).decode("utf-8")
         print(word)
         return word
 
-    def _get_embedding(self, word):
+    def _fetch_embedding(self, word):
         str_raw = r.get(word)
         if str_raw == None:
             # See ISSUE 8
@@ -51,14 +58,10 @@ class EmbeddingFeeder():
             return embedding
 
     def feed(self, cmd):
-
-        embedding = self._get_embedding(cmd)
+        embedding = self._fetch_embedding(cmd)
         
-        self.arr_point.pop(randint(0, NUM_WORD - 1))
-        i = self.seed
-        
+        self.arr_point.pop(randint(0, NUM_WORD - 1))        
         self.arr_point.append({"word": cmd, "embedding": embedding, "status": 1})
-        self._update_seed()
 
     def get_json_arr_point(self):
         print(self.arr_point)
