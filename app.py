e709121954e6278c50e5f9641e78acdcdae49bea
sanghunkang <sanghunkang.dev@gmail.com>
2018-05-24 23:05:56 +0900

    worked with embeddingFeeder.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-
 from embeddingFeeder import EmbeddingFeeder
 
 app = Flask(__name__, template_folder="dist", static_folder="dist/static")
@@ -12,17 +11,15 @@ def render_static():
 @app.route("/api/initialize", methods=["POST"])
 def api_initialize():
     print(request.get_json())
-    return embedding_feeder.get_json_arr_point()
+    # Here, only protocol related exception will be handled    
+    return embedding_feeder.get_arr_point()
 
 @app.route("/api/hit_enter", methods=["POST"])
 def hit_enter():
     print(request.get_json())
     cmd = request.get_json()["cmd"]
-    try:
-        embedding_feeder.feed(cmd)
-        return embedding_feeder.get_json_arr_point()
-    except:
-        return embedding_feeder.get_json_arr_point()
+    # Here, only protocol related exception will be handled
+    return embedding_feeder.get_arr_point_by_query(cmd)
  
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- embeddingFeeder.py
+++ embeddingFeeder.py
@@ -17,32 +17,35 @@ def parse_str2arr(str_raw):
 
 class EmbeddingFeeder():
     def __init__(self, initial_seed=10):
-        self.seed = initial_seed
-        self.arr_point = []
+        self.seed = randint(0, SIZE_VOCABULARY)
         self.arr_seed_used = []
-        # self.some_store for used keys
+        self.arr_point = []
 
         # Initialisation actions
         for i in range(NUM_WORD):
-            self._set_seed()
-            seed = self._get_seed()
-            word = self._fetch_random_word(seed)
+            word = self._fetch_random_word()
             embedding = self._fetch_embedding(word)
             self.arr_point.append({"word": word, "embedding": embedding, "status": 1})
 
     def _set_seed(self):
+        seed = randint(0, SIZE_VOCABULARY)
         # Some mechanism to control random seed including
         #   - check repeated inputs
         #   - update level
         if True:
-            self.seed = randint(0, SIZE_VOCABULARY)
+            self.seed = seed
+            self.arr_seed_used.append(seed)
+
+    def _set_word(self):
+        self.word = r.get(self.seed).decode("utf-8")
 
     def _get_seed(self):
         return self.seed
 
     def _fetch_random_word(self):
         self._set_seed()
-        word = r.get(self.seed).decode("utf-8")
+        seed = self._get_seed()
+        word = r.get(seed).decode("utf-8")
         print(word)
         return word
 
@@ -57,12 +60,17 @@ class EmbeddingFeeder():
             embedding = parse_str2arr(str_raw)
             return embedding
 
-    def feed(self, cmd):
-        embedding = self._fetch_embedding(cmd)
+    def get_arr_point_by_query(self, query):
+        # { Some actions to further process query }
+        query_processed = query
+        embedding = self._fetch_embedding(query_processed)
         
         self.arr_point.pop(randint(0, NUM_WORD - 1))        
-        self.arr_point.append({"word": cmd, "embedding": embedding, "status": 1})
-
-    def get_json_arr_point(self):
-        print(self.arr_point)
+        self.arr_point.append({
+            "word": query_processed,
+            "embedding": embedding,
+            "status": 1})
+        return jsonify({"arrBrick": self.arr_point})
+    
+    def get_arr_point(self):
         return jsonify({"arrBrick": self.arr_point})
